4d6292895571421aa9240aaa01975ed0a6397a27
ayurn <65171090+ayurn@users.noreply.github.com>
2021-04-26 18:14:41 +0530

    Added : Single player at position 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,5 +5,8 @@ public class SnakeAndLadder
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to SnakeAndLadder program");
+
+		int position=0;
+		System.out.println("Player is at position : "+position);
 	}
 }
