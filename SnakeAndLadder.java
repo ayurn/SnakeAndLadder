a6eceb7035067c8775fe399ba335bc9744829ccc
ayurn <65171090+ayurn@users.noreply.github.com>
2021-04-27 00:20:37 +0530

    Added : Gamr for two players and if player gets ladder they play again.

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,75 +1,102 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class SnakeAndLadder 
-{	
-	public static final int No_Play=1;
-	public static final int Ladder=2;
-	public static final int Snake=3;
-	public static final int Win_position=100;
-	public static final int Position_check = 0;
-	
-	public static void main(String[] args) 
-	{
-		System.out.println("Welcome to SnakeAndLadder program");
-		int noOfDieRoll = 0;
-		int position=0;
-		System.out.println("Start position of player :	"+position);
+public class SnakeAndLadder {
+    public static final int No_Play = 1;
+    public static final int Ladder = 2;
+    public static final int Snake = 3;
+    public static final int Win_position = 100;
+    public static final int Position_check = 0;
+    public static final int START_POSITION = 0;
 
-		while (position < Win_position){
+    public static void main(String[] args) {
+        System.out.println("Welcome to SnakeAndLadder program");
+        boolean player1IsPlaying = true;
+        int player1Position = START_POSITION, player2Position = START_POSITION, dieRoll, options, dieRolledTimes = 0;
+        Random random = new Random();
+        while (player1Position != Win_position && player2Position != Win_position) {
+            dieRoll = random.nextInt(6) + 1;
+            dieRolledTimes++;
+            options = random.nextInt(3) + 1;
 
-		//Rolling dice to get random number	
-		Random random = new Random();
-	    int droll = 0;
-		while (true)
-		{
-    		droll = random.nextInt(7);
-    		if(droll !=0) break;
-		}
-		System.out.println("Number of dice : "+droll);
+            switch (options) {
+                case No_Play:
+                    if (player1IsPlaying) {
+                        player1Position += 0;
+                        player1IsPlaying = false;
+                    }else{
+                        player2Position += 0;
+                        player1IsPlaying = true;
+                    }
+                    break;
+                case Ladder:
+                    if(player1IsPlaying) {
 
-		//options
+                        if (player1Position + dieRoll <= Win_position) {
 
-		Random ran = new Random();
-		noOfDieRoll++;
-	    int option = 0;
-		while (true)
-		{
-    		option = ran.nextInt(4);
-    		if(option !=0) break;
-		}
-		System.out.println("====OPTIONS======\nOption 1 : No Play\nOption 2 : Ladder \nOption 3 : Snake \n");
-		System.out.println("Option : "+option);
-		//Switch statement
-		switch (option) {
-			case No_Play:
-				System.out.println("Player stays in the same position : "+position);
-				break;
-			
-			case Ladder:
-				if ((position + droll) > Win_position){
-					position = Win_position;
-				} else{
-					position += droll;	
-				}
-				System.out.println("Player moves ahead by : "+position);
-				break;
-			
-			case Snake:
-                position -= droll;
-                if ((position - droll) < Position_check){
-                	position = Position_check;
-                }
-				System.out.println("Player moves behind by : "+position);
-				break;				
+                            player1Position += dieRoll;
+                            System.out.println("Player 1 landed on a ladder");
 
-			default: 
-				System.out.println("Enter correct value");
-				break;
-			}
-		}
-		System.out.println("Number Of Times The Dice Was Played To Win The Game Is : " +noOfDieRoll);
-	}
+                        }
 
+                    } else {
+
+                        if (player2Position + dieRoll <= Win_position) {
+
+                            player2Position += dieRoll;
+                            System.out.println("Player 2 landed on a ladder");
+
+                        }
+
+                    }
+                    break;
+                case Snake:
+                    if(player1IsPlaying) {
+
+                        player1Position -= dieRoll;
+                        player1IsPlaying = false;
+
+                        if (player1Position < 0) {
+
+                            player1Position = 0;
+
+                        }
+
+                        System.out.println("Player 1 landed on a snake");
+
+                    } else {
+
+                        player2Position -= dieRoll;
+                        player1IsPlaying = true;
+
+                        if (player2Position < 0) {
+
+                            player2Position = 0;
+
+                        }
+
+                        System.out.println("Player 2 landed on a snake");
+                        break;
+                    }
+                default:
+                    System.out.println("Enter proper value");
+                    break;
+
+            }
+            System.out.println("Player 1 position  = " + player1Position);
+            System.out.println("Player 2 position  = " + player2Position);
+
+        }
+
+        if(player1Position == 100) {
+
+            System.out.println("Player 1 won the game!");
+
+        } else {
+
+            System.out.println("Player 2 won the game!");
+
+        }
+        System.out.println("Dice was rolled " + dieRolledTimes + " to win the game");
+    }
 }
-
